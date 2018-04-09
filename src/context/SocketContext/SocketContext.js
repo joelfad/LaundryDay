643078a47b4ab6c970a704acab83f52102968485
Rowan Moul <Rowan.Moul@ucalgary.ca>
2018-04-08 19:31:59 -0600

    Update socketContext to pass socket, not object containing it.

--- src/context/SocketContext/SocketContext.js
+++ src/context/SocketContext/SocketContext.js
@@ -4,15 +4,19 @@ import io from "socket.io-client";
 const SocketContext = React.createContext();
 
 const socketProvider = (props) => (
-    <SocketContext.Provider value={{socket: io("localhost:3001")}}>
+    <SocketContext.Provider value={io("localhost:3001")}>
         {props.children}
     </SocketContext.Provider>
 );
 
-const withSocket = () => Component => (props) => (
-    <SocketContext.Consumer>
-        {(context) => <Component socket={context.socket} {...props}/>}
-    </SocketContext.Consumer>
-);
+const withSocket = () => Component => {
+    // Returning a named value tells react what to call this component. Otherwise it's just "Unknown" in the dev tools
+    const WithSocket = (props) => (
+        <SocketContext.Consumer>
+            {socket => <Component socket={socket} {...props}/>}
+        </SocketContext.Consumer>
+    );
+    return WithSocket;
+}
 
 export { socketProvider as SocketProvider, withSocket};
