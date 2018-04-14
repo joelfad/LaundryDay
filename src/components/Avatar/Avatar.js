9f45cc8af51c27e72a9664846ba58fa66e4a5877
Michael <mikegeeraert@gmail.com>
2018-04-13 21:15:11 -0600

    add 50 avatars

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { withStyles } from "material-ui/styles";
 import avatarStyles from "./styles";
-import avatar1 from './icons/1.svg';
-import avatar2 from './icons/2.svg';
-import avatar3 from './icons/3.svg';
-import avatar4 from './icons/4.svg';
-import avatar5 from './icons/5.svg';
-import avatar6 from './icons/6.svg';
 
-const avatars = [avatar1, avatar2, avatar3, avatar4, avatar5, avatar6];
+function importAll(r) {
+    let images = [];
+    r.keys().map((item, index) => { images.push(r(item)); });
+    return images;
+}
+
+const avatars = importAll(require.context('./icons', false, /\.svg/));
 
 const avatar = props => {
     let { classes } = props;
